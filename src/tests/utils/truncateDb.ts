ce57c499e21316e0b0ecc39dbca6faebc5ed1c49
Victor Hugo Simões <victorhugobs@eq.ufrj.br>
2024-04-06 00:40:09 -0300

    cry: execute function to truncate database

---
+++ src/tests/utils/truncateDb.ts
@@ -0,0 +1,17 @@
+import { prisma } from '@/utils/prisma'
+
+export async function truncateDB() {
+  const tablenames = await prisma.$queryRaw<
+    Array<{ tablename: string }>
+  >`SELECT tablename FROM pg_tables WHERE schemaname='public'`
+
+  for (const { tablename } of tablenames) {
+    if (tablename !== '_prisma_migrations') {
+      try {
+        await prisma.$executeRawUnsafe(`TRUNCATE TABLE "public"."${tablename}" CASCADE;`)
+      } catch (error) {
+        console.log({ error })
+      }
+    }
+  }
+}
