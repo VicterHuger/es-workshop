a2bdd73fe2821e8faa3a6dc92aa6211cc3407299
Victor Hugo Simões <victorhugobs@eq.ufrj.br>
2024-04-08 19:07:36 -0300

    feat: add branch info to commit created event

--- src/repository/events/commitCreated.ts
+++ src/repository/events/commitCreated.ts
@@ -6,6 +6,7 @@ export class CommitCreated implements Event {
   constructor(
     public streamId: string,
     public version: number,
+    public branch: { id: string; name: string },
     public actor?: string
   ) {}
 }
--- src/repository/events/index.ts
+++ src/repository/events/index.ts
@@ -1,3 +1,5 @@
 export { CommitCreated } from './commitCreated'
 export { CommitMessageAdded } from './commitMessagedAdded'
 export { CommitMessageRemoved } from './commitMessageRemoved'
+export { BranchCreated } from './branchCreated'
+export { BranchNameAdded } from './branchNameAdded'
