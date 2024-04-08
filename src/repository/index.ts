1997986f9b90fc8591d762c42eb9194644431621
Victor Hugo Simões <victorhugobs@eq.ufrj.br>
2024-04-08 19:09:45 -0300

    feat: add aggregates e middlewares to domain

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,6 +1,8 @@
-import { CommitProjectionAgreggate } from './aggregates'
-import { CreateCommitCommand, UpdateMessageCommitCommand } from './commands'
+import { BranchAgreggate, BranchProjectionAgreggate, CommitProjectionAgreggate } from './aggregates'
+import { CreateBranchCommand, CreateCommitCommand, UpdateMessageCommitCommand } from './commands'
 import { CommitFetcher } from './fetchers'
+import { BranchMiddleware } from './middlewares/branchMiddleware'
+import { BranchProjectionMiddleware } from './middlewares/branchProjectionMiddleware'
 import { CommitProjectionMiddleware } from './middlewares/commitProjectionMiddleware'
 import { ContextInitializerMiddleware } from './middlewares/contextInitializer'
 import { PersistenceMiddleware } from './middlewares/persistenceMiddleware'
@@ -10,10 +12,14 @@ function createDommain(command: Command): EventSourcingDomain {
   const domain = new EventSourcingDomain()
 
   domain.registerAggregate(new CommitProjectionAgreggate())
+  domain.registerAggregate(new BranchAgreggate())
+  domain.registerAggregate(new BranchProjectionAgreggate())
 
   domain.registerMiddleware(new ContextInitializerMiddleware())
   // adicionar middlewares aqui
   domain.registerMiddleware(new CommitProjectionMiddleware())
+  domain.registerMiddleware(new BranchMiddleware())
+  domain.registerMiddleware(new BranchProjectionMiddleware())
 
   domain.registerMiddleware(new PersistenceMiddleware())
 
@@ -22,10 +28,20 @@ function createDommain(command: Command): EventSourcingDomain {
   return domain
 }
 
+//CRIAR BRANCH
+export async function CreateBranch(actor: string, name: string): Promise<void> {
+  const createBranchCommand = new CreateBranchCommand(actor, name)
+
+  await createDommain(createBranchCommand).run()
+}
+
 //CRIAR COMMIT
-export async function CreateCommit(actor: string, message: string): Promise<void> {
-  const commitProjectionAgreggate = new CommitProjectionAgreggate()
-  const createCommitCommand = new CreateCommitCommand(actor, message).withAggregate(commitProjectionAgreggate)
+export async function CreateCommit(
+  actor: string,
+  message: string,
+  branch: { id: string; name: string }
+): Promise<void> {
+  const createCommitCommand = new CreateCommitCommand(actor, message, branch)
 
   await createDommain(createCommitCommand).run()
 }
