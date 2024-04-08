8b372487e9e49ceacd3eaf618a5a86bc314e9a9c
Victor Hugo Simões <victorhugobs@eq.ufrj.br>
2024-04-08 19:08:25 -0300

    feat: add branch middlewared

---
+++ src/repository/middlewares/branchMiddleware.ts
@@ -0,0 +1,46 @@
+import { BranchAgreggate } from '../aggregates'
+import type { ExtendedContext } from './contextInitializer'
+import { DomainMiddleware } from '@/utils/event_sourcing'
+
+export class BranchMiddleware extends DomainMiddleware {
+  public async apply(context: ExtendedContext): Promise<ExtendedContext> {
+    for (const agg of context.affectedAggregates.values()) {
+      if (!(agg instanceof BranchAgreggate)) continue
+
+      const replayed = await agg.replay(context.fetchedEventsfromCommandAggregate)
+
+      for (const event of context.events) {
+        await replayed.apply(event)
+      }
+
+      const state = replayed.getState()
+
+      let selector = ''
+      if (!state?.id) {
+        for (const event of context.events) {
+          const normalizedEvent = event as unknown as Event & Record<string, unknown>
+
+          if (
+            typeof normalizedEvent?.branch === 'object' &&
+            normalizedEvent?.branch &&
+            'id' in normalizedEvent.branch &&
+            typeof normalizedEvent?.branch.id === 'string'
+          ) {
+            selector = normalizedEvent?.branch?.id
+            break
+          }
+        }
+      }
+
+      context.projections.push({
+        collectionName: 'branch',
+        data: state,
+        selector: state?.id || selector,
+      })
+
+      break
+    }
+
+    return context
+  }
+}
